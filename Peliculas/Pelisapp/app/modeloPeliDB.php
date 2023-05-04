07e99181b5df3e2cfb11b82b20a3c6ccf1d6f088
AlfOrtGar <114944459+AlfOrtGar@users.noreply.github.com>
2023-05-04 12:04:44 +0200

    Update modeloPeliDB.php

--- Peliculas/Pelisapp/app/modeloPeliDB.php
+++ Peliculas/Pelisapp/app/modeloPeliDB.php
@@ -10,13 +10,6 @@ class modeloPeliDB {
      private static $modifica_peli = "Update peliculas set nombre = ?, director = ?, genero = ? where codigo_pelicula = ?";
      private static $borra_peli = "Delete from peliculas where codigo_pelicula = ?";
      private static $nueva_peli = "Insert into peliculas (codigo_pelicula,nombre,director,genero) Values (?,?,?,?)";
-  /*
-     private static $delete_peli   = "Delete from Usuarios where id = ?"; 
-     private static $insert_user   = "Insert into Usuarios (id,clave,nombre,email,plan,estado)".
-                                     " VALUES (?,?,?,?,?,?)";
-     private static $update_user    = "UPDATE Usuarios set  clave=?, nombre =?, ".
-                                     "email=?, plan=?, estado=? where id =?";
- */    
      
 public static function init(){
    
@@ -35,66 +28,6 @@ public static function init(){
     }
     
 }
-
-
-/***
-// Borrar un usuario (boolean)
-public static function UserDel($userid){
-    $stmt = self::$dbh->prepare(self::$delete_user);
-    $stmt->bindValue(1,$userid);
-    $stmt->execute();
-    if ($stmt->rowCount() > 0 ){
-        return true;
-    }
-    return false;
-}
-// Añadir un nuevo usuario (boolean)
-public static function UserAdd($userid, $userdat):bool{
-    $stmt = self::$dbh->prepare(self::$insert_user);
-    $stmt->bindValue(1,$userid);
-    $clave = Cifrador::cifrar($userdat[0]);
-    $stmt->bindValue(2,$clave);
-    $stmt->bindValue(3,$userdat[1] );
-    $stmt->bindValue(4,$userdat[2] );
-    $stmt->bindValue(5,$userdat[3] );
-    $stmt->bindValue(6,$userdat[4] );
-    if ($stmt->execute()){
-       return true;
-    }
-    return false; 
-}
-
-// Actualizar un nuevo usuario (boolean)
-// GUARDAR LA CLAVE CIFRADA
-public static function UserUpdate ($userid, $userdat){
-    $clave = $userdat[0];
-    // Si no tiene valor la cambio
-    if ($clave == ""){ 
-        $stmt = self::$dbh->prepare(self::$update_usernopw);
-        $stmt->bindValue(1,$userdat[1] );
-        $stmt->bindValue(2,$userdat[2] );
-        $stmt->bindValue(3,$userdat[3] );
-        $stmt->bindValue(4,$userdat[4] );
-        $stmt->bindValue(5,$userid);
-        if ($stmt->execute ()){
-            return true;
-        }
-    } else {
-        $clave = Cifrador::cifrar($clave);
-        $stmt = self::$dbh->prepare(self::$update_user);
-        $stmt->bindValue(1,$clave );
-        $stmt->bindValue(2,$userdat[1] );
-        $stmt->bindValue(3,$userdat[2] );
-        $stmt->bindValue(4,$userdat[3] );
-        $stmt->bindValue(5,$userdat[4] );
-        $stmt->bindValue(6,$userid);
-        if ($stmt->execute ()){
-            return true;
-        }
-    }
-    return false; 
-}
-****/
 
 public static function UltimoID (){
     $stmt=self::$dbh->query("Select MAX(codigo_pelicula) from peliculas");
@@ -169,29 +102,6 @@ public static function Actualizar($id,$titulo,$director,$genero){
     $stmt->execute();
 }
 
-// Datos de una película para visualizar
-public static function UserGet ($codigo){
-    $datosuser = [];
-    $stmt = self::$dbh->prepare(self::$consulta_user);
-    $stmt->bindValue(1,$userid);
-    $stmt->execute();
-    if ($stmt->rowCount() > 0 ){
-        // Obtengo un objeto de tipo Usuario, pero devuelvo una tabla
-        // Para no tener que modificar el controlador
-        $stmt->setFetchMode(PDO::FETCH_CLASS, 'Usuario');
-        $uobj = $stmt->fetch();
-        $datosuser = [ 
-                     $uobj->clave,
-                     $uobj->nombre,
-                     $uobj->email,
-                     $uobj->plan,
-                     $uobj->estado
-                     ];
-        return $datosuser;
-    }
-    return null;    
-    
-}
 
 public static function closeDB(){
     self::$dbh = null;
